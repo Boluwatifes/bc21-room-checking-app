c9b8d801110a8a97641ae3cc69fae11e9fc205ad
Bamidele <fisiwizy@gmail.com>
2017-04-13 18:40:49 +0100

    bug fixed: query value now received

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 app.get('/rooms/meetings', (req, res) => {
     let roomRef = dbRef.child('meetings');
     roomRef.on('value', (snap) => {
-        res.send(snap.val());
+        res.json(snap.val());
     });
 
 })
@@ -30,7 +30,7 @@ app.get('/rooms/meetings', (req, res) => {
 app.get('/rooms/games', (req, res) => {
     let roomRef = dbRef.child('game');
     roomRef.on('value', (snap) => {
-        res.send(snap)
+        res.json(snap)
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/rooms/games', (req, res) => {
 app.get('/rooms/working', (req, res) => {
     let roomRef = dbRef.child('working');
     roomRef.on('value', (snap) => {
-        res.send(snap.val())
+        res.json(snap.val())
     });
 
 });
@@ -47,7 +47,7 @@ app.get('/rooms/quiet', (req, res) => {
 
     let roomRef = dbRef.child('quiet');
     roomRef.on('value', (snap) => {
-        res.send(snap.val())
+        res.json(snap.val())
     })
 });
 
@@ -55,7 +55,7 @@ app.get('/rooms/learning', (req, res) => {
 
     let roomRef = dbRef.child('learning');
     roomRef.on('value', (snap) => {
-        res.send(snap.val())
+        res.json(snap.val())
     })
 });
 
